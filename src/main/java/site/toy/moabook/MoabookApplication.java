b95b6b60935e22faff42fb1a1bbd8b7a6f9c23e2
Lee SeungWoo <xonic789@naver.com>
2022-01-24 23:50:15 +0900

    spring init

---
+++ src/main/java/site/toy/moabook/MoabookApplication.java
@@ -0,0 +1,13 @@
+package site.toy.moabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoabookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/toy/moabook/MoabookApplicationTests.java
@@ -0,0 +1,13 @@
+package site.toy.moabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
